265df72ea018cb17b711a7176902801497428042
persianProgrammers <abdollamemar@gmail.com>
2025-07-06 16:16:12 +0330

    feat AB: latest-orders in home page inititialized

---
+++ Menro.Application/Restaurants/Services/Interfaces/ILatestOrdersCardService.cs
@@ -0,0 +1,14 @@
+﻿using Menro.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menro.Application.Restaurants.Services.Interfaces
+{
+    public interface ILatestOrdersCardService
+    {
+        Task<List<RestaurantCardDto>> GetLatestOrderedRestaurantCardsAsync(string userId);
+    }
+}
