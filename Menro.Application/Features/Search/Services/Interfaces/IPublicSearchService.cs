da22d512ec9d06334a092e2b65cad42542454b79
persianProgrammers <abdollamemar@gmail.com>
2026-02-19 13:16:42 +0330

    AB feat: search function

---
+++ Menro.Application/Features/Search/Services/Interfaces/IPublicSearchService.cs
@@ -0,0 +1,14 @@
+﻿using Menro.Application.Features.Search.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menro.Application.Features.Search.Services.Interfaces
+{
+    public interface IPublicSearchService
+    {
+        Task<SearchResponseDto> SearchAsync(string term, int take = 15);
+    }
+}
---
+++ Menro.Domain/Interfaces/ISearchRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Menro.Domain.Contracts;
+
+namespace Menro.Domain.Interfaces
+{
+    public interface ISearchRepository
+    {
+        Task<List<SearchHit>> SearchAsync(string term, int take);
+    }
+}
